7b47bc10bf20051cb78c17de56b5a4a3b97f8db5
sangwoong <sangwoong03@gmail.com>
2022-04-25 15:12:31 +0900

    2022-04-25

--- src/color.js
+++ src/color.js
@@ -1,6 +1,12 @@
-const btn = document.querySelector(".color-btn");
 const pallete = document.querySelector(".control__colors");
+const sliderBtn = document.querySelector(".slider.round");
+const sideSec = document.querySelector(".side--section");
+const title = document.querySelectorAll(".main--title");
+const navList = document.querySelectorAll(".nav--list");
+const colorController = document.querySelector(".color-controller");
+const btn = document.querySelector(".color-btn");
 
+// Color Change Module
 const COLOR_THEME = [
 	"#2c2c2c",
 	"#ffffff",
@@ -13,11 +19,12 @@ const COLOR_THEME = [
 	"#5856d6",
 ];
 
+// Create the Html element
 function palletMaker(color) {
 	return `<div class="controls__color" style="background: ${color} "}> </div>`;
 }
 
-btn.onclick = (e) => {
+btn.onclick = () => {
 	const classCheck = pallete.classList.contains("visible");
 
 	if (classCheck) {
@@ -27,3 +34,20 @@ btn.onclick = (e) => {
 		pallete.classList.add("visible");
 	}
 };
+
+document.addEventListener("click", (e) => {
+	const PAINT = "controls__color";
+
+	if (e.target && e.target.className === PAINT) {
+		const color = e.target.style.backgroundColor;
+		sideSec.style.borderRight = `1px solid ${color}`;
+		sliderBtn.style.backgroundColor = color;
+		container.style.boxShadow = `0 3px 10px ${color}, 0 1px 3px ${color}`;
+		for (let i = 0; i < navList.length; i++) {
+			navList[i].style.color = color;
+			title[i].style.color = color;
+		}
+	} else {
+		console.log("click the paint of pallet");
+	}
+});
--- src/mode.js
+++ src/mode.js
@@ -3,21 +3,41 @@ const moonImg = document.querySelector(".moon-image");
 const sunImg = document.querySelector(".sun-image");
 const body = document.querySelector("body");
 const container = document.querySelector(".container");
-
+const navItem = document.querySelectorAll(".nav--list");
+const catchingItem = document.querySelectorAll(".catching-item");
+console.log(navItem);
+// Mode Change Module
 slider.onclick = (e) => {
 	const checked = e.target.checked;
 	const bodyStyle = body.style;
+
 	if (!checked) {
 		moonImg.classList.remove("visible");
 		sunImg.classList.add("visible");
 		bodyStyle.backgroundColor = "#ffffff";
 		bodyStyle.color = "#060606";
+		container.style.backgroundColor = "#ffffff";
+		colorController.style.filter = `opacity(.5) drop-shadow(0 0 0 #000)`;
+		for (let i = 0; i < navItem.length; i++) {
+			navItem[i].style.color = "#2c2c2c";
+			if (i < 3) {
+				catchingItem[i].style.backgroundColor = "rgba(0, 0, 0, .1)";
+				continue;
+			}
+		}
 	} else {
 		moonImg.classList.add("visible");
 		sunImg.classList.remove("visible");
 		body.style.backgroundColor = "#000000";
 		bodyStyle.color = "#ffffff";
+		container.style.backgroundColor = "#1b1b1b";
+		colorController.style.filter = `opacity(0.9) drop-shadow(0 0 0 #fff)`;
+		for (let i = 0; i < navItem.length; i++) {
+			navItem[i].style.color = "#ffffff";
+			if (i < 3) {
+				catchingItem[i].style.backgroundColor = "rgba(0, 0, 0, .5)";
+				continue;
+			}
+		}
 	}
-	console.log(e.target.checked);
-	// checked = true or false일 때 >> background + color 전환
 };
