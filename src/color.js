c76d5cbb55a7b038dd23fc6ad2bc95dcfb300cb2
sangwoong <sangwoong03@gmail.com>
2022-05-16 17:34:25 +0900

    update js module for changing color themes

--- src/color.js
+++ src/color.js
@@ -7,37 +7,58 @@ const navList = document.querySelectorAll(".nav--list");
 const btn = document.querySelector(".color-btn");
 const containerDiv = document.querySelector(".container");
 const skillBar = document.querySelectorAll(".skill--bar");
+const inputSlider = document.querySelector("input");
 
 // Color Change Module
 const COLOR_THEME = [
-	"#2c2c2c",
-	"#e2e2e2",
-	"#ff3b30",
-	"#ff9500",
-	"#ffcc00",
-	"#4cd963",
-	"#5ac8fa",
-	"#0579ff",
-	"#5856d6",
+	{ id: 1, color: "#2c2c2c" },
+	{ id: 2, color: "#e2e2e2" },
+	{ id: 3, color: "#ff3b30" },
+	{ id: 4, color: "#ff9500" },
+	{ id: 5, color: "#ffcc00" },
+	{ id: 6, color: "#4cd963" },
+	{ id: 7, color: "#5ac8fa" },
+	{ id: 8, color: "#0579ff" },
+	{ id: 9, color: "#5856d6" },
 ];
 
-// Create the Html element
-function palletMaker(color) {
-	return `<div class="controls__color" style="background: ${color} "}> </div>`;
+function createPalletHTML(color) {
+	return `<div class="controls__color" style="background: ${color}" invisible}> </div>`;
 }
 
-btn.onclick = () => {
+function changePaint(e) {
+	const inputChecked = e.target.checked;
+
+	const COLOR_THEME_MODE = inputChecked
+		? COLOR_THEME.filter((item) => item.id !== 1)
+		: COLOR_THEME.filter((item) => item.id !== 2);
+
+	pallete.innerHTML = COLOR_THEME_MODE.map((item) =>
+		createPalletHTML(item.color),
+	).join("");
+}
+// Create the Html element
+
+function palletMaker(e) {
 	const classCheck = pallete.classList.contains("invisible");
+	const modeCheck = e.target.classList.contains("dark-mode");
 
 	if (classCheck) {
 		pallete.classList.remove("invisible");
-		pallete.innerHTML = COLOR_THEME.map((item) => palletMaker(item)).join("");
-		const paint = document.querySelectorAll(".controls__color");
-		console.log(paint);
+		if (!modeCheck) {
+			pallete.innerHTML = COLOR_THEME.filter((item) => item.id !== 2)
+				.map((paintColor) => createPalletHTML(paintColor.color))
+				.join("");
+		} else {
+			pallete.innerHTML = COLOR_THEME.filter((item) => item.id !== 1)
+				.map((paintColor) => createPalletHTML(paintColor.color))
+				.join("");
+		}
 	} else {
 		pallete.classList.add("invisible");
+		pallete.innerHTML = " ";
 	}
-};
+}
 
 function colorChanger(e) {
 	const PAINT = "controls__color";
@@ -53,7 +74,6 @@ function colorChanger(e) {
 		console.log("please click the paint");
 	}
 }
-document.addEventListener("click", colorChanger);
 
 function navListTitleColor(color) {
 	for (let i = 0; i < navList.length; i++) {
@@ -67,3 +87,7 @@ function skillBarColor(color) {
 		skillBar[i].style.backgroundColor = color;
 	}
 }
+
+btn.addEventListener("click", palletMaker);
+inputSlider.addEventListener("click", changePaint);
+document.addEventListener("click", colorChanger);
